72943621ef3afd21feed9e67a95bd5f31dbd3064
ihrtrdio <67624620+ihrtrdio@users.noreply.github.com>
2020-06-29 23:25:37 -0700

    My first ever C program
    I learned vim editor 
    I learned to compile using gcc
    I learned to use ssh shell to linux

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf ("\n My first C program\n");
+    printf ("\n My Username In Zombsroyale is psdn.ihrtrdio\n");
+    printf ("\n I Want To Be Good At C Programming\n");
+    printf ("\n I'm Bored In The House And Don't Know What To Do!\n");
+    printf ("\n *****");
+    printf ("\n  ***");
+    printf ("\n   *\n");
+
+    return 0;
+}
