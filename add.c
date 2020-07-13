e7de800a30aa1bc408d69b7cedc367b1ca5cfdd4
Leon Mathew <leon@matt-lnx.attlocal.net>
2020-07-12 21:05:49 -0700

    My second C program

---
+++ add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{ 
+	int first;
+	int second;
+	int sum;
+	
+	printf("\n Key in first number:");
+
+	scanf("%d",&first);
+
+	printf(" Key in second number:");
+
+	scanf("%d",&second);
+
+	sum=first+second;
+	printf("\nsum=%d\n",sum);
+
+	return 0;
+
+}
+
